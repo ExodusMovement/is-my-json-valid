1ed5513a33a43f2026ce307f838ea2fa6037735b
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-05-27 16:18:47 +0300

    refactor json-schema test runner
    Now has an explicit blacklist of unsupported tests,
    which can consist of files, blocks, or individual tests

--- test/json-schema.js
+++ test/json-schema.js
@@ -3,26 +3,36 @@ const fs = require('fs')
 const path = require('path')
 const validator = require('../')
 
-const schemaDraftDir = path.join(__dirname, '/json-schema-draft4')
-const files = fs
-  .readdirSync(schemaDraftDir)
-  .map(function(file) {
-    if (file === 'definitions.json') return null
-    if (file === 'refRemote.json') return null
-    if (file === 'ref.json') return null
-    const content = fs.readFileSync(path.join(schemaDraftDir, file))
-    return JSON.parse(content)
-  })
-  .filter(Boolean)
+const unsupported = new Set([
+  // Files
+  'definitions.json',
+  'refRemote.json',
+  'ref.json',
+])
 
-files.forEach(function(file) {
-  file.forEach(function(f) {
-    tape(`json-schema-test-suite ${f.description}`, function(t) {
-      const validate = validator(f.schema)
-      f.tests.forEach(function(test) {
+const schemaDraftDir = path.join(__dirname, '/json-schema-draft4')
+
+function processTestDir(subdir = '') {
+  const dir = path.join(schemaDraftDir, subdir)
+  for (const file of fs.readdirSync(dir)) {
+    if (unsupported.has(path.join(subdir, file))) continue
+    const content = fs.readFileSync(path.join(dir, file))
+    processTest(JSON.parse(content))
+  }
+}
+
+function processTest(file) {
+  for (const block of file) {
+    if (unsupported.has(block.description)) continue
+    tape(`json-schema-test-suite ${block.description}`, (t) => {
+      const validate = validator(block.schema)
+      for (const test of block.tests) {
+        if (unsupported.has(test.description)) continue
         t.same(validate(test.data), test.valid, test.description)
-      })
+      }
       t.end()
     })
-  })
-})
+  }
+}
+
+processTestDir()
