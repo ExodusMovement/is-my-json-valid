3511a1643d4001e356ef96ca4419f5b3d61c8086
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-05-29 04:53:44 +0300

    formats: remove style format, minor cleanup

--- formats.js
+++ formats.js
@@ -1,3 +1,9 @@
+/**
+ * NOTICE
+ * 'regex' delibirately not supported due to potential DoS. Don't accept regexes from untrusted sources.
+ * 'style' also delibirately removed
+ */
+
 exports[
   'date-time'
 ] = /^\d{4}-(?:0[0-9]{1}|1[0-2]{1})-[0-9]{2}[tT ]\d{2}:\d{2}:\d{2}(?:\.\d+|)([zZ]|[+-]\d{2}:\d{2})$/
@@ -10,25 +16,23 @@ exports[
 ] = /(#?([0-9A-Fa-f]{3,6})\b)|(aqua)|(black)|(blue)|(fuchsia)|(gray)|(green)|(lime)|(maroon)|(navy)|(olive)|(orange)|(purple)|(red)|(silver)|(teal)|(white)|(yellow)|(rgb\(\s*\b([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\b\s*,\s*\b([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\b\s*,\s*\b([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\b\s*\))|(rgb\(\s*(\d?\d%|100%)+\s*,\s*(\d?\d%|100%)+\s*,\s*(\d?\d%|100%)+\s*\))/
 exports['hostname'] = function(input) {
   if (!/^[a-zA-Z0-9.-]+$/.test(input)) return false
-
   const parts = input.split('.')
-
-  for (let i = 0; i < parts.length; i++) {
-    if (!/^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9])$/.test(parts[i])) return false
-  }
-
-  return true
+  return parts.every((part) =>
+    /^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9])$/.test(part)
+  )
 }
-exports['host-name'] = exports['hostname'] // draft3 backwards compat
 exports['alpha'] = /^[a-zA-Z]+$/
 exports['alphanumeric'] = /^[a-zA-Z0-9]+$/
-exports['style'] = /\s*(.+?):\s*([^;]+);?/g
 exports['phone'] = function(input) {
   if (!/^\+[0-9][0-9 ]{5,27}[0-9]$/.test(input)) return false
   if (/ {2}/.test(input)) return false
-
   const digits = input.substring(1).replace(/ /g, '').length
-
   return digits >= 7 && digits <= 15
 }
 exports['utc-millisec'] = /^[0-9]{1,15}\.?[0-9]{0,15}$/
+
+/**
+ * Compatibility formats
+ */
+
+exports['host-name'] = exports['hostname'] // draft3 backwards compat
